90a3fe94b5f51e2d086c4fb4a54580326c4dedf3
bbbro <romanbbbrrr@gmail.com>
2024-03-21 00:45:10 +0100

    completed task 3

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def generate_squares(start, end):
-    return [x ** 2 for x in range(start, end + 1)]
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x ** 2 for x in range(start, end + 1)]
 
 
+square_generator = SquareGenerator()
 start_range = 1
 end_range = 10
-squares_list = generate_squares(start_range, end_range)
+squares_list = square_generator.generate_squares(start_range, end_range)
+
 print(squares_list)
