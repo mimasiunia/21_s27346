df43dac27fd72b860e7403bf421015b1ebf0b929
bbbro <romanbbbrrr@gmail.com>
2024-03-21 00:48:12 +0100

    completed task 4

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import math
+
+
 class SquareGenerator:
     def generate_squares(self, start, end):
         return [x ** 2 for x in range(start, end + 1)]
+
+    def square_roots(self, squares_list):
+        return [math.sqrt(x) for x in squares_list]
 
 
 square_generator = SquareGenerator()
@@ -8,4 +14,7 @@ start_range = 1
 end_range = 10
 squares_list = square_generator.generate_squares(start_range, end_range)
 
-print(squares_list)
+square_roots_list = square_generator.square_roots(squares_list)
+
+print(f"Squares List: {squares_list}")
+print(f"Square Roots List: {square_roots_list}")
